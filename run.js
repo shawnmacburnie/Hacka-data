8d5406a0dd3633d5a9c61f98ee2c73e28fbacbf7
sookool99 <shawnmacburnie@gmail.com>
2015-07-13 21:24:02 -0300

    inc search by 20.

--- run.js
+++ run.js
@@ -2,7 +2,6 @@
 var fs = require('fs'),
     categorys = "",
     location = "",
-    total = 0,
     pwd = process.cwd();
 
 var yelp = require("yelp").createClient({
@@ -22,10 +21,12 @@ if (process.argv.length < 3) {
     return;
 }
 function find() {
+    var searchCompleted = 0,
+        total = 0;
     yelp.search({
         category_filter: categorys,
         location: location,
-        radius_filter: 39000
+        sort: 2,
     }, function(error, data) {
         if (error) return console.log(error)
         data = data.businesses;
@@ -47,11 +48,48 @@ function find() {
                 if (message.replace(/ /g, "").length) fs.appendFileSync(pwd + "/data.csv", message.replace(/undefined/g,"") +  '\n');
             }
         }
-        console.log("Finished!")
-        console.log("File Saved to: " + pwd + "/data.csv")
-        console.log("Total items added: " + total);
+        if(++searchCompleted == 2) {
+            console.log("Finished!")
+            console.log("File Saved to: " + pwd + "/data.csv")
+            console.log("Total items added: " + total);
+        }
+    });
+    yelp.search({
+        category_filter: categorys,
+        location: location,
+        radius_filter: 39000,
+        sort: 2,
+        limit: 20,
+        offset: 20
+    }, function(error, data) {
+        if (error) return console.log(error)
+        data = data.businesses;
+        total += data.length;
+        for (var i = 0; i < data.length; i++) {
+            var item = data[i];
+            if(item && item.name && item.location && item.location.coordinate) {
+                var message = item.name.replace(/,/g, "") + ',';
+
+                if(typeof(item.location.address) === "object") {
+                    for(var kk in item.location.address){
+                        message += item.location.address[kk].replace(/,/g, "") + " ";
+                    }
+                } else {
+                    message += item.location.address + " ";
+                }
+                message += item.location.city + " " + item.location.state_code + " " + item.location.postal_code + ',';
+                message += item.phone + ',' + (item.snippet_text ? item.snippet_text.replace(/,|\n/g, "") : '') + ',' + item.location.coordinate.latitude + ',' + item.location.coordinate.longitude +',' + categorys.replace(/,/g, "") + ',' + item.id;
+                if (message.replace(/ /g, "").length) fs.appendFileSync(pwd + "/data.csv", message.replace(/undefined/g,"") +  '\n');
+            }
+        }
+        if(++searchCompleted == 2) {
+            console.log("Finished!")
+            console.log("File Saved to: " + pwd + "/data.csv")
+            console.log("Total items added: " + total);
+        }
     });
 }
+
 
 process.argv.some(function(val, index, array) {
     if (index >= 2) {
