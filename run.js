fde3fe51112d3c3be8e8d584ee404c406966a2cb
sookool99 <shawnmacburnie@gmail.com>
2015-07-13 21:31:34 -0300

    reformatted code.

--- run.js
+++ run.js
@@ -2,6 +2,8 @@
 var fs = require('fs'),
     categorys = "",
     location = "",
+    searchCompleted = 0,
+    total = 0,
     pwd = process.cwd();
 
 var yelp = require("yelp").createClient({
@@ -21,46 +23,19 @@ if (process.argv.length < 3) {
     return;
 }
 function find() {
-    var searchCompleted = 0,
-        total = 0;
-    yelp.search({
-        category_filter: categorys,
-        location: location,
-        sort: 2,
-    }, function(error, data) {
-        if (error) return console.log(error)
-        data = data.businesses;
-        total += data.length;
-        for (var i = 0; i < data.length; i++) {
-            var item = data[i];
-            if(item && item.name && item.location && item.location.coordinate) {
-                var message = item.name.replace(/,/g, "") + ',';
+    yelpQuery();
+    yelpQuery(20);
+}
 
-                if(typeof(item.location.address) === "object") {
-                    for(var kk in item.location.address){
-                        message += item.location.address[kk].replace(/,/g, "") + " ";
-                    }
-                } else {
-                    message += item.location.address + " ";
-                }
-                message += item.location.city + " " + item.location.state_code + " " + item.location.postal_code + ',';
-                message += item.phone + ',' + (item.snippet_text ? item.snippet_text.replace(/,|\n/g, "") : '') + ',' + item.location.coordinate.latitude + ',' + item.location.coordinate.longitude +',' + categorys.replace(/,/g, "") + ',' + item.id;
-                if (message.replace(/ /g, "").length) fs.appendFileSync(pwd + "/data.csv", message.replace(/undefined/g,"") +  '\n');
-            }
-        }
-        if(++searchCompleted == 2) {
-            console.log("Finished!")
-            console.log("File Saved to: " + pwd + "/data.csv")
-            console.log("Total items added: " + total);
-        }
-    });
+function yelpQuery(offset) {
+    if(!offset) offset = 0;
     yelp.search({
         category_filter: categorys,
         location: location,
         radius_filter: 39000,
         sort: 2,
         limit: 20,
-        offset: 20
+        offset: offset
     }, function(error, data) {
         if (error) return console.log(error)
         data = data.businesses;
@@ -83,13 +58,12 @@ function find() {
             }
         }
         if(++searchCompleted == 2) {
-            console.log("Finished!")
-            console.log("File Saved to: " + pwd + "/data.csv")
+            console.log("Finished!");
+            console.log("File Saved to: " + pwd + "/data.csv");
             console.log("Total items added: " + total);
         }
     });
 }
-
 
 process.argv.some(function(val, index, array) {
     if (index >= 2) {
