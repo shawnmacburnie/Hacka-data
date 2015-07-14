a16858311b95b44677e8ac43c0d28fa2d4b42d12
sookool99 <shawnmacburnie@gmail.com>
2015-07-14 10:33:44 -0300

    created functionality for many search. #3

--- run.js
+++ run.js
@@ -3,9 +3,9 @@
 var fs = require('fs'),
     categorys = "",
     location = "",
-    searchCompleted = 0,
-    total = 0,
+    findSearch = false,
     pwd = process.cwd();
+
 var yelp = require("yelp").createClient({
     consumer_key: process.env.yelp_consumer_key,
     consumer_secret: process.env.yelp_consumer_secret,
@@ -18,19 +18,30 @@ function find() {
     yelpQuery(20);
 }
 
-function yelpQuery(offset) {
+function findMany() {
+    var manyCateg = categorys.split(",");
+
+    for (var i = 0; i < manyCateg.length;i++) {
+        console.log("Running Query for " + manyCateg[i]);
+        yelpQuery(0,manyCateg[i]);
+        yelpQuery(20, manyCateg[i]);
+    }
+}
+
+function yelpQuery(offset, categ) {
     if (!offset) offset = 0;
+    if (!categ) categ = categorys
     yelp.search({
-        category_filter: categorys,
+        category_filter: categ,
         location: location,
         radius_filter: 40000,
         sort: 2,
         limit: 20,
         offset: offset
     }, function(error, data) {
-        if (error) return console.log(error)
+        if (error) return console.log(error);
         data = data.businesses;
-        total += data.length;
+        if(!data.length) return;
         for (var i = 0; i < data.length; i++) {
             var item = data[i];
             if (item && item.name && item.location && item.location.coordinate) {
@@ -44,35 +55,40 @@ function yelpQuery(offset) {
                     message += item.location.address + " ";
                 }
                 message += item.location.city + " " + item.location.state_code + " " + item.location.postal_code + ',';
-                message += item.phone + ',' + (item.snippet_text ? item.snippet_text.replace(/,|\n/g, "") : '') + ',' + item.location.coordinate.latitude + ',' + item.location.coordinate.longitude + ',' + categorys.replace(/,/g, "") + ',' + item.id;
+                message += item.phone + ',' + (item.snippet_text ? item.snippet_text.replace(/,|\n/g, "") : '') + ',' + item.location.coordinate.latitude + ',' + item.location.coordinate.longitude + ',' + categ.replace(/,/g, "") + ',' + item.id;
                 if (message.replace(/ /g, "").length) fs.appendFileSync(pwd + "/data.csv", message.replace(/undefined/g, "") + '\n');
             }
         }
-        if (++searchCompleted == 2) {
-            console.log("Finished!");
-            console.log("File Saved to: " + pwd + "/data.csv");
-            console.log("Total items added: " + total);
-        }
+        console.log(data.length + " items added.");
     });
 }
 
 fs.writeFileSync(pwd + "/data.csv", 'name,civicAddress,phoneNumber,description,latitude,longitude,type,buisnessId\n');
 
-if (process.argv.length < 3) {
-    return console.log("ERROR:\tMust pass a location as first parameter!");
-} else if (process.argv.length < 4) {
-    console.log("ERROR:\tMust pass atleast 1 category after location");
-    process.stdout.write("ERROR:\tTo Find All Category's: https://www.yelp.com/developers/documentation/v2/all_category_list");
-    return;
+if (process.argv.length < 4) {
+    return console.log("ERROR:\tMust pass required paremeters");
 }
 
 process.argv.some(function(val, index, array) {
     if (index >= 2) {
         if (index == 2) {
-            location = val;
+            if (val === "many"){
+                findSearch = true;
+            } else {
+                location = val;
+            }
         } else {
-            categorys += (index == 3 ? '' : ',') + val;
+            if (findSearch) {
+                if(index == 3) {
+                    location = val;
+                } else {
+                    categorys += (index == 4 ? '' : ',') + val;
+                }
+            } else {
+                categorys += (index == 3 ? '' : ',') + val;
+            }
         }
     }
 });
-find();
+
+findSearch ? findMany(): find();
